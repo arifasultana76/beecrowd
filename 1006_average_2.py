69677ba145c3203583aef0a938e45e64a69c8c8e
arifasultana76 <arifa.rimi76@gmail.com>
2024-12-26 15:22:28 +0600

    https://judge.beecrowd.com/en/problems/view/1006

---
+++ 1006_average_2.py
@@ -0,0 +1,11 @@
+a = float(input())
+b = float(input())
+c = float(input())
+
+weight_a = 2
+weight_b = 3
+weight_c = 5
+
+average = (a* weight_a + b* weight_b + c* weight_c) / (weight_a + weight_b + weight_c)
+
+print(f"MEDIA = {average:.1f}")
