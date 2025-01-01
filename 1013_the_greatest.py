4a3d9d2fc99704016587d7f30d7ea3494c2db9a7
arifasultana76 <arifa.rimi76@gmail.com>
2025-01-01 23:15:25 +0600

    https://judge.beecrowd.com/en/problems/view/1013

--- 1013_the_greatest.py
+++ 1013_the_greatest.py
@@ -1,6 +1,6 @@
 a,b,s = map(int,input().split())
 
-MaiorAB = (a+b+abs(a-b)) // 2
-MaiorFinal = (MaiorAB + s + abs(MaiorAB - s)) // 2   
-
-print(f"{MaiorFinal} eh o maior")
+maiorAB = (a+b+abs(a-b)) // 2           # double slash dile floating digit ashbe na. intiger digit a ans hobe.
+maiorFinal = (maiorAB + s + abs(maiorAB - s)) // 2   #greatest ber korte bolle ae 2 ta formula use hobe. 
+                                                  
+print(f"{maiorFinal} eh o maior")         # here's abs substractor(jog biyog korar por jodi digit er age minus value ashe tokhn sei minus r ashbe na.)
