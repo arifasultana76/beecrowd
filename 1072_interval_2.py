137abb2552b870d1dbdf0d649705271b535c34a1
arifasultana76 <arifa.rimi76@gmail.com>
2025-01-19 00:29:29 +0600

    https://judge.beecrowd.com/en/problems/view/1072

---
+++ 1072_interval_2.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+a = 0
+b = 0
+
+for i in range(n):
+    x = int(input())
+
+    if 10 <= x <= 20:
+        a += 1
+    else:
+        b += 1
+
+print(f"{a} in")
+print(f"{b} out")
