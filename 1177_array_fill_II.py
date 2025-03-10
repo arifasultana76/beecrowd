f3db54c27179990c124d5204a918bd275172178e
arifasultana76 <arifa.rimi76@gmail.com>
2025-03-10 12:09:01 +0600

    https://judge.beecrowd.com/en/problems/view/1177

---
+++ 1177_array_fill_II.py
@@ -0,0 +1,13 @@
+t = int(input())
+j = 0
+
+for i in range(1000):
+
+    if j < t:
+        print(f"N[{i}] = {j}")
+        j += 1
+
+    else:
+        j = 0
+        print(f"N[{i}] = {j}")
+        j += 1
