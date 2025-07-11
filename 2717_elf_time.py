7913e7bb746370fd4a8c389406cb2360a43bf423
arifasultana76 <arifa.rimi76@gmail.com>
2025-07-12 00:03:56 +0600

    https://judge.beecrowd.com/en/problems/view/2717

---
+++ 2717_elf_time.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        n = int(input())
+        a, b = map(int, input().split())
+
+        if a + b <= n:
+            print("Farei hoje!")
+        else:
+            print("Deixa para amanha!")
+    except EOFError:
+        break
